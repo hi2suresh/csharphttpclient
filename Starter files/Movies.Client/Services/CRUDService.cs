8b5c06789b4309327541a7f754a4461cd7ad1322
hi2suresh <hi2suresh@gmail.com>
2021-10-24 09:42:09 +0800

    Added GetSync Method

--- Starter files/Movies.Client/Services/CRUDService.cs
+++ Starter files/Movies.Client/Services/CRUDService.cs
@@ -13,25 +13,26 @@ namespace Movies.Client.Services
 {
     public class CRUDService : IIntegrationService
     {
-        private static HttpClient _httpCLient = new HttpClient();
+        private static HttpClient _httpClient = new HttpClient();
 
         public CRUDService()
         {
-            _httpCLient.BaseAddress = new Uri("http://localhost:57863");
-            _httpCLient.Timeout = new TimeSpan(0, 0, 30);
-            _httpCLient.DefaultRequestHeaders.Clear();
-            _httpCLient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            _httpCLient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml", 0.9));
+            _httpClient.BaseAddress = new Uri("http://localhost:57863");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+            //_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //_httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml", 0.9));
 
         }
         public async Task Run()
         {
-            await GetResource();
+            //await GetResource();
+            await GetResourceThroughHttpRequestMessage();
         }
 
         public async Task GetResource()
         {
-            var response = await _httpCLient.GetAsync("api/movies");
+            var response = await _httpClient.GetAsync("api/movies");
             response.EnsureSuccessStatusCode();
             var movies = new List<Movie>();
            var content = await response.Content.ReadAsStringAsync();
@@ -42,13 +43,33 @@ namespace Movies.Client.Services
                    {
                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
                    });
-            } else if (response.Content.Headers.ContentType.MediaType == 'application/xml')
+            } else if (response.Content.Headers.ContentType.MediaType == "application/xml")
             {
                 var serializer = new XmlSerializer(typeof(List<Movie>));
                 movies = (List<Movie>)serializer.Deserialize(new StringReader(content));
 
             }
      
+        }
+
+        public async Task GetResourceThroughHttpRequestMessage()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/movies");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var movies = JsonSerializer.Deserialize<IEnumerable<Movie>>(content,
+                           new JsonSerializerOptions
+                           {
+                               PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                           }
+                         );
+
+
         }
     }
 }
